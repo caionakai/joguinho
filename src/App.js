e50bef51e49a3cbf3341ad04e92c13a6628aaecf
caionakai <kaioesperto@hotmail.com>
2018-11-23 00:32:37 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
+import Cadastro from './Cadastro'
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavItem, NavDropdown, NavbarBrand, Nav, MenuItem } from 'react-bootstrap';
+import { BrowserRouter , Switch, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">SOAP</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="/cadastro">
+                Cadastro
+              </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>;
+
+
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,11 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/sobre" component={Sobre} />
+        </Switch>
+    </ BrowserRouter>);
   ReactDOM.unmountComponentAtNode(div);
 });
