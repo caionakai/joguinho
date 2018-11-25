c04e0058d92649ea714baa2725ca00b3244b4b98
caionakai <kaioesperto@hotmail.com>
2018-11-25 19:53:52 -0200

    tst tipo complexo

--- src/Cadastro.js
+++ src/Cadastro.js
@@ -7,41 +7,53 @@ class Cadastro extends React.Component {
     super(props, context);
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
     this.msgServidor = this.msgServidor.bind(this);
 
     this.state = {
-      value: ''
+      a: '',
+      b: '',
     };
   }
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-    return null;
-  }
+  // getValidationState() {
+  //   const length = this.state.value.length;
+  //   if (length > 10) return 'success';
+  //   else if (length > 5) return 'warning';
+  //   else if (length > 0) return 'error';
+  //   return null;
+  // }
 
   handleChange(e) {
-    this.setState({ value: e.target.value });
+    this.setState({ a: e.target.value });
+  }
+  handleChange2(e) {
+    this.setState({ b: e.target.value });
   }
 
   msgServidor(){
-var soap = require('soap-everywhere');
-var url = 'http://localhost:8001/wscalc1?wsdl';
-
-soap.createClient(url, function(err, client) {
-    if (err) throw err;
-    console.log(client.describe().ws.calc);
-    client.multiplicar({a: 4,b: 3},function(err,res){
-        if (err) throw err;
-        console.log(res);
-    });
-    client.sumar({a: 4,b: 3},function(err,res){
-        if (err) throw err;
-        console.log(res);
-    });
-});
+	var soap = require('soap-everywhere');
+	var url = 'http://localhost:8001/wscalc1?wsdl';
+	var param = {
+		a: this.state.a,
+		b: this.state.b
+	};
+	soap.createClient(url, function(err, client) {
+	    if (err) throw err;
+	    console.log(client.describe().ws.calc);
+	    client.multiplicar(param,function(err,res){
+	        if (err) throw err;
+	        console.log(res);
+	    });
+	    client.sumar({a: 4,b: 3},function(err,res){
+	        if (err) throw err;
+	        console.log(res);
+	    });
+	    client.devolvenome({nome: 'caio'},function(err,res){
+	         if (err) throw err;
+	        console.log(res);  	
+	    });
+	});
 }
 
   render() {
@@ -50,15 +62,21 @@ soap.createClient(url, function(err, client) {
 	      <form>
 	        <FormGroup
 	          controlId="formBasicText"
-	          validationState={this.getValidationState()}
+	          // validationState={this.getValidationState()}
 	        >
 	        <ControlLabel>Teste</ControlLabel>
 	        <FormControl
 	            type="text"
 	            value={this.state.value}
-	            placeholder="Digite aqui"
+	            placeholder="Digite um numero"
 	            onChange={this.handleChange}
 	        />
+	        <FormControl
+	            type="text"
+	            value={this.state.b}
+	            placeholder="Digite outro numero"
+	            onChange={this.handleChange2}
+	        />	        
 	        <FormControl.Feedback />
 	        <Button id="btn" onClick={this.msgServidor} bsStyle="info">Enviar</Button>
 	        </FormGroup>
--- src/Server.js
+++ src/Server.js
@@ -4,6 +4,23 @@
 var soap = require('soap');
 var http = require('http');
 
+var usuarios = []
+var mapa = []
+function create_mapa(){
+    for (var x = 0; x < 30; x++) {
+        let linha = []
+        for(var y = 0; y< 30; y++){
+            linha.unshift('X')
+        } 
+        mapa.unshift(linha)
+    }
+}
+create_mapa()
+var teste = [1,2,3]
+// function create_usario(nome){
+//     usuarios.unshift({nome: nome, map_position= [0,0]})
+// }
+
 var service = {
     ws: {
         calc: {
@@ -15,8 +32,17 @@ var service = {
             multiplicar : function(args) {
                 var n = args.a * args.b;
                 return { mulres : n };
+            },
+            devolvenome : function(){
+                var nomeres = "Caio"
+                return { nomeres };
+            },
+            criarusuario : function(nome){
+                create_usario(nome);
+                return { teste }
             }
-        }
+        },
+
     }
 };
 
@@ -25,6 +51,6 @@ var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
 var server = http.createServer(function(request,response) {
     response.end("404: Not Found: "+request.url);
 });
-
 server.listen(8001);
 soap.listen(server, '/wscalc1', service, xml);
+console.log("Servidor ligado..")
