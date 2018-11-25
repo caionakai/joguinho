6a600868c7aa3517c600a19d47c7da0cf6909740
Gabriel Choptian <choptian@alunos.utfpr.edu.br>
2018-11-25 19:51:06 -0200

    Tipo Complexo Usuario

--- src/Cadastro.js
+++ src/Cadastro.js
@@ -32,12 +32,10 @@ var url = 'http://localhost:8001/wscalc1?wsdl';
 
 soap.createClient(url, function(err, client) {
     if (err) throw err;
+
     console.log(client.describe().ws.calc);
-    client.multiplicar({a: 4,b: 3},function(err,res){
-        if (err) throw err;
-        console.log(res);
-    });
-    client.sumar({a: 4,b: 3},function(err,res){
+
+    client.GetUser({name:'Gabriel'},function(err,res){
         if (err) throw err;
         console.log(res);
     });
--- src/Server.js
+++ src/Server.js
@@ -4,17 +4,19 @@
 var soap = require('soap');
 var http = require('http');
 
+
+var user = {
+    name: 'Gabriel',
+    map_x: 0,
+    map_y: 0
+};
+
 var service = {
     ws: {
         calc: {
-            sumar : function(args) {
-                var n = 1*args.a + 1*args.b;
-                return { sumres : n };
-            },
-
-            multiplicar : function(args) {
-                var n = args.a * args.b;
-                return { mulres : n };
+            GetUser : function (name) {
+                console.log(name);
+                return {User: user}
             }
         }
     }
