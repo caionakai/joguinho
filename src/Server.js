4322334259863885e81d5300004b76500749be51
Gabriel Choptian <choptian@alunos.utfpr.edu.br>
2018-11-25 21:02:10 -0200

    Create Map

--- src/Server.js
+++ src/Server.js
@@ -5,12 +5,29 @@ const soap = require('soap');
 const http = require('http');
 
 let userList = [];
-
+let map = [];
 let user = {
     name: 'Gabriel',
     map_x: 0,
     map_y: 0
 };
+
+function create_map() {
+    for (let i = 0; i < 30; i++) {
+        for (let j = 0; j < 30; j++) {
+            map.append({x: i, y: j, usuario: null})
+        }
+    }
+}
+
+create_map();
+
+function load_map(user) {
+    return map.filter(position => {
+        return Math.abs(position.x - user.map_x) <= 2 && Math.abs(position.y - user.map_y) <= 2
+    })
+}
+
 
 userList.unshift(user);
 userList.unshift(user);
@@ -27,6 +44,9 @@ let service = {
             },
             GetUserList: function (name) {
                 return {Users: userList}
+            },
+            GetMap: function (user) {
+                return {map: load_map(user)}
             }
         }
     }
