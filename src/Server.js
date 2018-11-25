c204aba94bba4957922e081a7d801c4fdd2957c1
Gabriel Choptian <choptian@alunos.utfpr.edu.br>
2018-11-25 20:01:27 -0200

    Testando mensagem Array

--- src/Server.js
+++ src/Server.js
@@ -1,31 +1,37 @@
 /*jslint node: true */
 "use strict";
 
-var soap = require('soap');
-var http = require('http');
+const soap = require('soap');
+const http = require('http');
 
+let userList = [];
 
-var user = {
+let user = {
     name: 'Gabriel',
     map_x: 0,
     map_y: 0
 };
 
-var service = {
+userList.unshift(user);
+
+let service = {
     ws: {
         calc: {
-            GetUser : function (name) {
+            GetUser: function (name) {
                 console.log(name);
                 return {User: user}
+            },
+            GetUserList: function () {
+                return {Users: userList}
             }
         }
     }
 };
 
-var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+const xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
 
-var server = http.createServer(function(request,response) {
-    response.end("404: Not Found: "+request.url);
+let server = http.createServer(function (request, response) {
+    response.end("404: Not Found: " + request.url);
 });
 
 server.listen(8001);
