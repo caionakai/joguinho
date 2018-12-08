e0b3a18b510aa0cf8c8f731933879c42302d4b79
caionakai <kaioesperto@hotmail.com>
2018-12-08 00:55:02 -0200

    - file test + tutorial

--- src/home.js
+++ src/home.js
@@ -5,7 +5,11 @@ class Home extends React.Component {
     render() {
         return (
             <div >
-                Ola Mundo
+                <h1>Tutorial</h1>
+                <p>Cada personagem possui um range para andar pelo mapa encontrando monstros e 
+                    outros usuários. O objetivo é derrotar monstros para conseguir dinheiro,
+                    armas e poções a fim de derrotar outros usuários.
+                </p>
             </div>
         );
     }
